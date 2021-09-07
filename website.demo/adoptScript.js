616c1a18d0df5d97ff85ae4e5ccf744f3a4237d1
amanjotgill23 <amanjotgill23@gmail.com>
2021-09-07 00:23:37 -0230

    Added styling and JavaScript to Adopt page

---
+++ website.demo/adoptScript.js
@@ -0,0 +1,11 @@
+function spcaEast(){
+    window.open("https://spcastjohns.org/adopt/");
+}
+
+function spcaWest(){
+    window.open("https://www.nlwestspca.com/adoptable");
+}
+
+function beaglePaws(){
+    window.open("https://www.beaglepaws.com/adopt");
+}
