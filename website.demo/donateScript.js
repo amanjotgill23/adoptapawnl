377fba8c2793fe6e3babd336f4b757124789934f
amanjotgill23 <amanjotgill23@gmail.com>
2021-09-07 00:27:22 -0230

    Added donation links using JavaScript and complex styling

---
+++ website.demo/donateScript.js
@@ -0,0 +1,11 @@
+function spcaEast(){
+    window.open("https://spcastjohns.org/donate/");
+}
+
+function spcaWest(){
+    window.open("https://www.nlwestspca.com/donate");
+}
+
+function beaglePaws(){
+    window.open("https://www.beaglepaws.com/donate");
+}
